1948d488fdbd4f0bfa76d52dafa1dfc2a090f504
Nazariy Mandebura <nazar.mandebura@gmail.com>
2021-04-06 20:11:09 +0200

    ihor: version 7.5

--- run.py
+++ run.py
@@ -2,7 +2,4 @@
 if __name__ == '__main__':
     for _ in range(10):
         # ihor change
-        print('version 6')
-        print('version 6')
-        print('version 6')
-        print('version 6')
+        print('version 7.5')
