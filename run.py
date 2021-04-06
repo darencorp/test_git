dfd48984bb91d9e903e319722c465589bf744fc1
darencorp <nazariy.mandebura@landingi.com>
2021-04-06 20:01:31 +0200

    version 3

--- run.py
+++ run.py
@@ -1,4 +1,7 @@
 
 if __name__ == '__main__':
     for _ in range(10):
-        print('version 2')
+        print('version 3')
+        print('version 3')
+        print('version 3')
+        print('version 3')
