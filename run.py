6fe1f9737adeb793c56ca102258f7423535b8730
darencorp <nazariy.mandebura@landingi.com>
2021-04-06 20:11:26 +0200

    version 7

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 
 if __name__ == '__main__':
     for _ in range(10):
-        # ihor change
-        print('version 6')
-        print('version 6')
-        print('version 6')
-        print('version 6')
+        # nazar change
+        print('version 7')
+        print('version 7')
+        print('version 7')
+        print('version 7')
